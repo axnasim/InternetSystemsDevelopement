8ac78a108d5f42c7266a6495197cb8efea39e14c
Cooper <cgpendleton@outlook.com>
2016-03-15 21:29:09 -0500

    add tree output utility

--- src/main/java/com/uno/app/Main.java
+++ src/main/java/com/uno/app/Main.java
@@ -9,7 +9,7 @@ import org.apache.commons.io.FileUtils;
 public class Main{
 	public static void main(String [] args) {
 		
-		if(args.length != 1){
+		if(args.length < 1){
 			throw new RuntimeException("Needs a pom file to read");
 		}
 		MavenAccessObject mao = new MavenAccessObject();
@@ -25,12 +25,29 @@ public class Main{
 		}
 		ArrayList<PackageElement> list = mao.getDependencies(args[0], parent);
 		
+		if (args.length > 1 && args[1].equals("-p")) {
+			System.out.println("\n--DEPENDENCY TREE--\n");
+			printTree(list, 0);
+			System.out.println();
+		}
+		
 		try {
 			FileUtils.deleteDirectory(new File("pomdump"));
 			FileUtils.deleteDirectory(new File("jardump"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	
+	private static void printTree(ArrayList<PackageElement> list, int depth) {
+		//Print each item, then recursively print their children
+		for (PackageElement pkg : list) {
+			for (int i = 0; i < depth; i++) {
+				System.out.print("...");
+			}
+			System.out.println(pkg.getArtifactID() + "-" + pkg.getVersion());
+			printTree(pkg.getDependencies(), depth + 1);
 		}
 	}
 }
