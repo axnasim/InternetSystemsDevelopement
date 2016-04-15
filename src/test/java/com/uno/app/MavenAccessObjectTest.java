9c0b79705f2e06baddce9ac650e90b1a474f9049
Joseph Eley <jeley@unomaha.edu>
2016-04-15 18:51:49 -0500

    Making tests more robust

--- src/test/java/com/uno/app/MavenAccessObjectTest.java
+++ src/test/java/com/uno/app/MavenAccessObjectTest.java
@@ -55,13 +55,16 @@ public class MavenAccessObjectTest {
     	mao.getDependencies(s, pe, 0);
     	File poms = new File("pomdump");
     	File[] files = poms.listFiles();
-    	System.out.println(files.length);
-    	assertEquals(files[0].getPath(), "pomdump/junit-4.11.pom");
-    	assertEquals(files[1].getPath(), "pomdump/commons-io-2.4.pom");
-    	assertEquals(files[2].getPath(), "pomdump/hamcrest-core-1.3.pom");
-    	assertEquals(files[3].getPath(), "pomdump/junit-4.10.pom");
-    	assertEquals(files[4].getPath(), "pomdump/hamcrest-core-1.1.pom");
-    	assertEquals(files[5].getPath(), "pomdump/logging-0.0.3.pom");
+    	String fileNames = "";
+    	for(int i = 0; i < files.length; i++){
+    		fileNames += files[i].getPath();
+    	}
+    	assertTrue(fileNames.contains("pomdump/junit-4.11.pom"));
+    	assertTrue(fileNames.contains("pomdump/commons-io-2.4.pom"));
+    	assertTrue(fileNames.contains("pomdump/hamcrest-core-1.3.pom"));
+    	assertTrue(fileNames.contains("pomdump/junit-4.10.pom"));
+    	assertTrue(fileNames.contains("pomdump/hamcrest-core-1.1.pom"));
+    	assertTrue(fileNames.contains("pomdump/logging-0.0.3.pom"));
         
         
     }
@@ -78,13 +81,16 @@ public class MavenAccessObjectTest {
     	mao.getDependencies(s, pe, 0);
     	File poms = new File("pomdump");
     	File[] files = poms.listFiles();
-    	System.out.println(files.length);
-    	assertEquals(files[0].getPath(), "pomdump/junit-4.11.pom");
-    	assertEquals(files[1].getPath(), "pomdump/commons-io-2.4.pom");
-    	assertEquals(files[2].getPath(), "pomdump/hamcrest-core-1.3.pom");
-    	assertEquals(files[3].getPath(), "pomdump/junit-4.10.pom");
-    	assertEquals(files[4].getPath(), "pomdump/hamcrest-core-1.1.pom");
-    	assertEquals(files[5].getPath(), "pomdump/logging-0.0.3.pom");
+    	String fileNames = "";
+    	for(int i = 0; i < files.length; i++){
+    		fileNames += files[i].getPath();
+    	}
+    	assertTrue(fileNames.contains("pomdump/junit-4.11.pom"));
+    	assertTrue(fileNames.contains("pomdump/commons-io-2.4.pom"));
+    	assertTrue(fileNames.contains("pomdump/hamcrest-core-1.3.pom"));
+    	assertTrue(fileNames.contains("pomdump/junit-4.10.pom"));
+    	assertTrue(fileNames.contains("pomdump/hamcrest-core-1.1.pom"));
+    	assertTrue(fileNames.contains("pomdump/logging-0.0.3.pom"));
         
         
     }
@@ -102,12 +108,17 @@ public class MavenAccessObjectTest {
     	File poms = new File("pomdump");
     	File[] files = poms.listFiles();
     	System.out.println(files.length);
-    	assertEquals(files[0].getPath(), "pomdump/junit-4.11.pom");
-    	assertEquals(files[1].getPath(), "pomdump/commons-io-2.4.pom");
-    	assertEquals(files[2].getPath(), "pomdump/hamcrest-core-1.3.pom");
-    	assertEquals(files[3].getPath(), "pomdump/junit-4.10.pom");
-    	assertEquals(files[4].getPath(), "pomdump/hamcrest-core-1.1.pom");
-    	assertEquals(files[5].getPath(), "pomdump/logging-0.0.3.pom");
+    	String fileNames = "";
+    	for(int i = 0; i < files.length; i++){
+    		fileNames += files[i].getPath();
+    	}
+    	assertTrue(fileNames.contains("pomdump/junit-4.11.pom"));
+    	assertTrue(fileNames.contains("pomdump/commons-io-2.4.pom"));
+    	assertTrue(fileNames.contains("pomdump/hamcrest-core-1.3.pom"));
+    	assertTrue(fileNames.contains("pomdump/junit-4.10.pom"));
+    	assertTrue(fileNames.contains("pomdump/hamcrest-core-1.1.pom"));
+    	assertTrue(fileNames.contains("pomdump/logging-0.0.3.pom"));
+
         
         
     }
@@ -128,9 +139,13 @@ public class MavenAccessObjectTest {
     	for(int i = 0; i < files.length; i++){
     		System.out.println(files[i].getPath());
     	}
-    	assertEquals(files[0].getPath(), "output/hamcrest-core-1.1.spdx");
-    	assertEquals(files[1].getPath(), "output/logging-0.0.3.spdx");
-    	assertEquals(files[2].getPath(), "output/hamcrest-core-1.3.spdx");
+    	String fileNames = "";
+    	for(int i = 0; i < files.length; i++){
+    		fileNames += files[i].getPath();
+    	}
+    	assertTrue(fileNames.contains("output/hamcrest-core-1.1.spdx"));
+    	assertTrue(fileNames.contains("output/logging-0.0.3.spdx"));
+    	assertTrue(fileNames.contains("output/hamcrest-core-1.3.spdx"));
         
         
     }
